e6b4c4e0d77554ff9ef6f076c7dd9f01a0b2d9f4
GunnarI <gunnarinasp@gmail.com>
2019-04-10 20:19:48 +0200

    Finished data_structure and data_manager

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import data_manager as dm
+#import matplotlib as mpl
+import matplotlib.pyplot as plt
+import filters as filt
+
+my_dm = dm.DataManager(r'C:\Users\Gunnar\Google Drive\MSThesis\Data')
+my_dm.update_data_structure()
+emg_data_dict = my_dm.load_emg('Subject01','20190405')
 
 
-my_dm = DataManager()
+for column in emg_data_dict["20190405 Subject01 Walk07"][:,1:].T:
+    plt.figure()
+    plt.plot(emg_data_dict["20190405 Subject01 Walk07"][:,0],column)
+
+    plt.plot(emg_data_dict["20190405 Subject01 Walk07"][:,0],filt.butter_bandpass_filter(column,20,200,1000,order=5))
+
+plt.show()
