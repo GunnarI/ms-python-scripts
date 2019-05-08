22e2420d1fdc25b1577393cc668fd7332987ae21
GunnarI <gunnarinasp@gmail.com>
2019-05-08 23:08:26 +0200

    Improved generalization for some methods
    Signal can now be filtered for specific subject from dictionary. Saves time if filtered signal already loaded.

--- filters.py
+++ filters.py
@@ -62,7 +62,17 @@ def normalize_emg(emg_data_dict, max_emg_dict):
     return emg_data_dict
 
 
-def min_max_normalize_data(df, norm_emg=True, norm_torque=False):
+def min_max_normalize_data(df, secondary_df=None, norm_emg=True, norm_torque=False):
+    """Normalizes EMG data and/or torque data from pandas.DataFrame using sklearn.preprocessing.MinMaxScaler.
+    If secondary_df is given then the scaling of the "primary" is done using the min and max from the "secondary"
+    DataFrame. This is useful when scaling the test dataset.
+
+    :param df: The dataframe to scale
+    :param secondary_df: Dataframe to use for the min/max values, if None then the primary df is used (default: None)
+    :param norm_emg: If True then emg values in df are normalized (default: True)
+    :param norm_torque: If True then torque values in df are normalized (default: False)
+    :return: Dataframe with normalized values
+    """
     return_df = df.copy()
     col = list(df)
     if 'Time' in col:
@@ -73,12 +83,35 @@ def min_max_normalize_data(df, norm_emg=True, norm_torque=False):
         exercise = return_df.pop('Exercise')
 
     emg = return_df.values
-    # for i in range(x[:,1:1+num_features], 1):
+
+    if secondary_df is None:
+        if norm_emg:
+            emg_scaler = preprocessing.MinMaxScaler(feature_range=(0, 1)).fit(emg)
+        if norm_torque:
+            torque_scaler = preprocessing.MinMaxScaler(
+                feature_range=((torque.min()/torque.max()), 1)).fit(torque.values.reshape(-1, 1))
+    else:
+        scaling_df = secondary_df.copy()
+
+        if 'Time' in col:
+            scaling_df.pop('Time')
+        if 'Torque' in col:
+            s_torque = scaling_df.pop('Torque')
+        if 'Exercise' in col:
+            scaling_df.pop('Exercise')
+
+        s_emg = scaling_df.values
+
+        if norm_emg:
+            emg_scaler = preprocessing.MinMaxScaler(feature_range=(0, 1)).fit(s_emg)
+        if norm_torque:
+            torque_scaler = preprocessing.MinMaxScaler(
+                feature_range=((s_torque.min()/s_torque.max()), 1)).fit(s_torque.values.reshape(-1, 1))
+
     if norm_emg:
-        return_df = pd.DataFrame(preprocessing.MinMaxScaler().fit_transform(emg),
-                                 columns=return_df.columns, index=return_df.index)
+        return_df = pd.DataFrame(emg_scaler.transform(emg), columns=return_df.columns, index=return_df.index)
     if norm_torque:
-        torque = preprocessing.MinMaxScaler(feature_range=(-1, 1))
+        torque = torque_scaler.transform(torque.values.reshape(-1, 1))
 
     if 'Time' in col:
         return_df.insert(0, 'Time', time)
@@ -93,82 +126,63 @@ def min_max_normalize_data(df, norm_emg=True, norm_torque=False):
 # def filter_emg_rt(channels, low_pass, high_pass, window)
 
 
-def filter_torque(torque_data_dict, filt_order, lowcut, fs, axis_of_focus=0, cut_time=False, lp_filter=False):
+def filter_torque(torque_data_dict, filt_order, lowcut, fs, axis_of_focus=0, cut_time=False, lp_filter=False,
+                  subject_id=None):
     torque_filt_data_dict = {}
     headers = {}
     b, a = butter(filt_order, lowcut, btype='lowpass', fs=fs)
     for key in torque_data_dict:
-        filtered_torque = np.zeros(shape=(len(torque_data_dict[key]["data"][:, 0]), 2))
-        filtered_torque[:, 0] = torque_data_dict[key]["data"][:, 0]
-        if lp_filter:
-            filtered_torque[:, 1] = lfilter(b, a, torque_data_dict[key]["data"][:, axis_of_focus + 1])
-        else:
-            filtered_torque[:, 1] = torque_data_dict[key]["data"][:, axis_of_focus + 1]
+        if subject_id is None or subject_id in key:
+            filtered_torque = np.zeros(shape=(len(torque_data_dict[key]["data"][:, 0]), 2))
+            filtered_torque[:, 0] = torque_data_dict[key]["data"][:, 0]
+            if lp_filter:
+                filtered_torque[:, 1] = lfilter(b, a, torque_data_dict[key]["data"][:, axis_of_focus + 1])
+            else:
+                filtered_torque[:, 1] = torque_data_dict[key]["data"][:, axis_of_focus + 1]
 
-        if cut_time and 'walk' in key.lower():
-            t1 = torque_data_dict[key]["t1"]
-            t2 = torque_data_dict[key]["t2"]
-            filtered_torque = filtered_torque[(filtered_torque[:, 0] >= t1) & (filtered_torque[:, 0] <= t2), :]
+            if cut_time and 'walk' in key.lower():
+                t1 = torque_data_dict[key]["t1"]
+                t2 = torque_data_dict[key]["t2"]
+                filtered_torque = filtered_torque[(filtered_torque[:, 0] >= t1) & (filtered_torque[:, 0] <= t2), :]
 
-        torque_filt_data_dict[key + ' filtered'] = filtered_torque
-        headers[key + ' filtered'] = ['Time', 'Torque']
+            torque_filt_data_dict[key + ' filtered'] = filtered_torque
+            headers[key + ' filtered'] = ['Time', 'Torque']
 
     # Save the filtered torque signals
     save_np_dict_to_txt(torque_filt_data_dict, './data/labels/', data_fmt='%f', headers=headers)
 
 
-def filter_emg(emg_data_dict, lowcut, highcut, window, fs, cut_time=False, fs_mean_window=0):
+def filter_emg(emg_data_dict, lowcut, highcut, window, fs, cut_time=False, fs_mean_window=0, subject_id=None):
     emg_filt_data_dict = {}
     emg_headers = {}
-    # max_emg_values_dict = {}
-    # max_emg_exercises_dict = {}
     for key in emg_data_dict:
-        ids = key.split()
-        num_emg = len(emg_data_dict[key]["data"][0, :]) - 1
-        t = emg_data_dict[key]["data"][:, 0]
-        t_short = t_vec_after_ma(window, t)
-        if fs_mean_window > 0:
-            t_short = t_short[fs_mean_window-1::fs_mean_window]
-        filtered_emg = np.zeros(shape=(len(t_short), num_emg + 1))
-        filtered_emg[:, 0] = t_short
-        for i, column in enumerate(emg_data_dict[key]["data"][:, 1:].T):
-            column = noise_filter(column, lowcut, highcut, fs)
-            column = demodulation(column)
-            column, t = smoothing(column, t, window, fs)
-            column = relinearization(column)
+        if subject_id is None or subject_id in key:
+            num_emg = len(emg_data_dict[key]["data"][0, :]) - 1
+            t = emg_data_dict[key]["data"][:, 0]
+            t_short = t_vec_after_ma(window, t)
             if fs_mean_window > 0:
-                tail = - (column.size % fs_mean_window) if column.size % fs_mean_window > 0 else column.size
-                column = column[:tail].reshape(-1, fs_mean_window).mean(axis=-1)
-            filtered_emg[:, i + 1] = column
+                t_short = t_short[fs_mean_window-1::fs_mean_window]
+            filtered_emg = np.zeros(shape=(len(t_short), num_emg + 1))
+            filtered_emg[:, 0] = t_short
+            for i, column in enumerate(emg_data_dict[key]["data"][:, 1:].T):
+                column = noise_filter(column, lowcut, highcut, fs)
+                column = demodulation(column)
+                column, t = smoothing(column, t, window, fs)
+                column = relinearization(column)
+                if fs_mean_window > 0:
+                    tail = - (column.size % fs_mean_window) if column.size % fs_mean_window > 0 else column.size
+                    column = column[:tail].reshape(-1, fs_mean_window).mean(axis=-1)
+                filtered_emg[:, i + 1] = column
 
-        emg_filt_data_dict[key + ' filtered'] = filtered_emg
+            emg_filt_data_dict[key + ' filtered'] = filtered_emg
 
-        emg_headers[key + ' filtered'] = emg_data_dict[key]["headers"]
+            emg_headers[key + ' filtered'] = emg_data_dict[key]["headers"]
 
-        # max_emg_key = ids[0] + ' ' + ids[1] + ' MaxEMG'
-
-        # If dict key for specific trial does not exist then create key and initialize array/list values
-        # if max_emg_key not in max_emg_values_dict:
-        #     max_emg_values_dict[max_emg_key] = np.zeros(num_emg)
-        #     max_emg_exercises_dict[max_emg_key] = [''] * num_emg
-
-        # Update max emg values from relevant trial with values from this loops exercise
-        # max_emg_values_dict[max_emg_key], max_emg_exercises_dict[max_emg_key] = get_max_emg_array(
-        #     max_emg_values_dict[max_emg_key],
-        #     max_emg_exercises_dict[max_emg_key],
-        #     filtered_emg[:, 1:], ids[2], 'walk')
-
-        if cut_time and 'walk' in key.lower():
-            t1 = emg_data_dict[key]["t1"]
-            t2 = emg_data_dict[key]["t2"]
-            emg_filt_data_dict[key + ' filtered'] = filtered_emg[
-                                                    (filtered_emg[:, 0] >= t1) & (filtered_emg[:, 0] <= t2), :]
-
-    # Save the max emg values to a txt file
-    # save_np_dict_to_txt(max_emg_values_dict, './data/', data_fmt='%f', headers=max_emg_exercises_dict)
-
-    # Normalize the emg data
-    # emg_filt_data_dict = normalize_emg(emg_filt_data_dict, max_emg_values_dict)
+            if cut_time and 'walk' in key.lower():
+                t1 = emg_data_dict[key]["t1"]
+                t2 = emg_data_dict[key]["t2"]
+                emg_filt_data_dict[key + ' filtered'] = filtered_emg[
+                                                        (filtered_emg[:, 0] >= t1) & (filtered_emg[:, 0] <= t2), :]
 
     # Save the filtered and normalized emg signals
     save_np_dict_to_txt(emg_filt_data_dict, './data/', data_fmt='%f', headers=emg_headers)
