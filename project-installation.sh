ebbf9f7b1bbf1bce9504ff0e002122ecf9d5722a
GunnarI <gunnarinasp@gmail.com>
2019-08-18 22:39:57 +0200

    Script to setup venv and install packages from requirements

---
+++ project-installation.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+python3 -m venv ./venv				# To create a virtual environment
+source ./venv/Scripts/activate		# To activate the virtual environment
+pip install -r ./requirements.txt	# To install all required packages
